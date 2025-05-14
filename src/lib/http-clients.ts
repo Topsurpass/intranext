dd774c6e95c60b1fc97cf8fafe15b40428289d46
Topsurpass <temitopeabiodun685@gmail.com>
2025-05-14 02:45:23 +0100

    resolved git conflict

--- src/lib/http-clients.ts
+++ src/lib/http-clients.ts
@@ -3,110 +3,18 @@ import axios from 'axios';
 import config from '@/@config';
 import { jwtDecode } from 'jwt-decode';
 import useAuthStore from '@/store/user-store';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-
-// PUBLIC HTTP client
-=======
 import { setAuthTokenHTTP } from '@/lib/set-auth-token';
 
-
->>>>>>> develop
-=======
-import { setAuthTokenHTTP } from '@/lib/set-auth-token';
-
-
->>>>>>> develop
 export const HTTP = axios.create({
 	baseURL: config.baseUrl,
 	timeout: config.httpTimeout,
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Authenticated HTTP client : for request that require accessToken
-=======
->>>>>>> develop
-=======
->>>>>>> develop
 const AuthHTTP = axios.create({
 	baseURL: config.baseUrl,
 	timeout: config.httpTimeout,
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Function to refresh the access token
-export const refreshAccessToken = async () => {
-  const addUserToStore = useAuthStore.getState().setUser;
-  const logout = useAuthStore.getState().reset;
-  try {
-		const { accessToken } = useAuthStore.getState();
-		const res = await HTTP.post('/api/auth/refresh/', {
-			refreshToken: accessToken,
-		});
-		const { token, ...rest } = res.data.data;
-		const decodedToken = jwtDecode(token);
-		addUserToStore({
-			token,
-			...rest,
-			...decodedToken,
-		} as any);
-		return token;
-  } catch (err: any) {
-		logout();
-		return err?.response?.data?.detail;
-		// return Promise.reject(err);
-  }
-};
-
-AuthHTTP.interceptors.request.use(
-  async (setting: any) => {
-    const token = useAuthStore.getState().accessToken;
-    if (token !== null && token !== undefined && token) {
-      setting.headers.Authorization = `Bearer ${token}`;
-      return setting;
-    }
-    return setting;
-  },
-  (err: any) => {
-    return Promise.reject(err);
-  }
-);
-
-AuthHTTP.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const logout = useAuthStore.getState().reset;
-    const originalRequest = error.config;
-
-    if (error.response && error.response.status === 401 && !originalRequest._retry) {
-      try {
-        originalRequest._retry = true;
-        const token = await refreshAccessToken();
-        // Retry the original request with the new access token
-        originalRequest.headers.Authorization = `Bearer ${token}`;
-        return await HTTP(originalRequest);
-        // return await AuthHTTP(originalRequest);
-      } catch (refreshError) {
-        logout();
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
-
-
-export default AuthHTTP;
-=======
-=======
->>>>>>> develop
 const isTokenExpired = (token: string): boolean => {
 	try {
 		const decoded: any = jwtDecode(token);
@@ -117,8 +25,7 @@ const isTokenExpired = (token: string): boolean => {
 };
 
 export const refreshAccessToken = async () => {
-	const { setUser, reset, refreshToken } =
-		useAuthStore.getState();
+	const { setUser, reset, refreshToken } = useAuthStore.getState();
 
 	if (!refreshToken || isTokenExpired(refreshToken)) {
 		reset();
@@ -127,14 +34,9 @@ export const refreshAccessToken = async () => {
 
 	try {
 		const res = await HTTP.post('/api/auth/refresh/', {
-			refresh: refreshToken 
-			}
-		);
-		const {
-			access,
-			refresh,
-			...rest
-		} = res.data;
+			refresh: refreshToken,
+		});
+		const { access, refresh, ...rest } = res.data;
 		setAuthTokenHTTP(access);
 
 		const decodedToken = jwtDecode(access);
@@ -194,7 +96,3 @@ AuthHTTP.interceptors.response.use(
 );
 
 export default AuthHTTP;
-<<<<<<< HEAD
->>>>>>> develop
-=======
->>>>>>> develop
