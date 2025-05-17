d405cf8c477d5b64a02c8c6bf9743260d9371619
Topsurpass <temitopeabiodun685@gmail.com>
2025-05-17 02:06:25 +0100

    added dummy exam-data file

---
+++ src/data/exam-data.ts
@@ -0,0 +1,74 @@
+export const ExamData = [
+	{
+		id: 1,
+		question: 'What is the capital of France?',
+		options: ['London', 'Berlin', 'Paris', 'Madrid'],
+		correctAnswer: 'Paris',
+	},
+	{
+		id: 2,
+		question: 'Which planet is known as the Red Planet?',
+		options: ['Venus', 'Mars', 'Jupiter'],
+		correctAnswer: 'Mars',
+	},
+	];
+
+
+	export const DynamicExamData = {
+		id: '1c4f6f16-4996-4fbe-8866-c8d2be484f98',
+		title: 'Basis of Web Application Exam',
+		description:
+			'This test your knowledge of all that you read and did on frontend development. For this examination, expects the following',
+		questions: [
+			{
+				id: '784a7ae7-238d-460f-aff1-b0547084050b',
+				text: 'What is  the full meaning of CSS ?',
+				options: [
+					{
+						id: '8494e0f8-f8dd-4e86-bf88-3eb0b6076284',
+						text: 'Currently serving server',
+					},
+					{
+						id: '8960df57-d43c-4e57-be8e-fad54c734eaf',
+						text: 'Come and serve swet',
+					},
+					{
+						id: '97059963-7e23-4549-9b60-adfed0989267',
+						text: 'Cascade stylesheet',
+					},
+				],
+			},
+			{
+				id: 'b36b8eca-92a2-43b7-be99-a6e0e7668d1b',
+				text: 'Can you build a beautiful looking  website using only html  only?',
+				options: [
+					{
+						id: '58d79a01-7d56-493c-aada-2372603e8431',
+						text: 'No',
+					},
+					{
+						id: 'b3d05487-b1a7-40da-99b0-c08803dfa1d2',
+						text: 'Yes',
+					},
+				],
+			},
+			{
+				id: 'ec30bd4b-54dc-4606-b76b-daf24d755c2d',
+				text: 'What is the full meaning of HTML ?',
+				options: [
+					{
+						id: '79372da9-3375-4038-a6cc-f5e2341bdf17',
+						text: 'Hypertex markup language',
+					},
+					{
+						id: '8014fbfe-5135-4bbf-9102-4e1fac2759e9',
+						text: 'How The Mike Leave',
+					},
+					{
+						id: 'ae63973d-be11-45c6-a7d2-635f8518b74d',
+						text: 'How To Make Love',
+					},
+				],
+			},
+		],
+	};
