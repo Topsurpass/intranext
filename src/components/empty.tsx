b172097dd6dec4411ebf2d6de0f0b1e5b46d4bcf
Topsurpass <temitopeabiodun685@gmail.com>
2025-05-21 15:55:03 +0100

    adjusted icon size

--- src/components/empty.tsx
+++ src/components/empty.tsx
@@ -9,8 +9,8 @@ type Props = {
 export default function Empty({ title, description, Icon }: Props) {
 	return (
 		<div className="col-span-full flex w-full flex-col items-center justify-center p-3 md:p-8 text-center">
-			<Icon className="mb-4 h-5 w-5 md:h-10 md:w-10 text-muted-foreground" />
-			<h2 className="md:text-lg text-sm font-semibold text-muted-foreground">
+			<Icon className="mb-4 h-8 w-8 md:h-10 md:w-10 text-muted-foreground" />
+			<h2 className="md:text-lg text-md font-semibold text-muted-foreground">
 				{title}
 			</h2>
 			<p className="mt-2 text-xs md:text-sm text-muted-foreground max-w-md">
