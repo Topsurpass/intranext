f8b1fcda3283f857e3f7ec6fe0cccf3e751317a0
Topsurpass <temitopeabiodun685@gmail.com>
2025-05-21 03:36:08 +0100

    made profile page mobile responsive

--- src/app/(private)/my_profile/page.tsx
+++ src/app/(private)/my_profile/page.tsx
@@ -144,7 +144,7 @@ export default function Page() {
 					) : (
 						<>
 							<div className="flex items-center justify-between gap-2">
-								<div className="flex items-center gap-5 justify-center">
+								<div className="flex items-center gap-5  flex-wrap">
 									<ProfileImageUpload
 										previewUrl={previewImage}
 										disabled={isProfileDisabled}
